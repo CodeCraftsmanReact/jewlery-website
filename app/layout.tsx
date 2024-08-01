8be585dd1651a519884879b748bbbda234254410
Joseph Kertz <lonewolfcode2@gmail.com>
2024-08-01 15:28:42 -0500

    optional

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import Navigation from "@/components/navigation";
 import Cart from "@/components/cart-provider";
+import PageTransitionEffect from "./PageTransitionEffect";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,17 +20,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} min-h-[100vh] relative`}>
-        <Cart>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <Navigation />
-            {children}
-          </ThemeProvider>
-        </Cart>
+        <PageTransitionEffect>
+          <Cart>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <Navigation />
+              {children}
+            </ThemeProvider>
+          </Cart>
+        </PageTransitionEffect>
       </body>
     </html>
   );
