dabbf9b0a4b2e5eeba50d88dbef85273f05cd4a6
Joseph Kertz <lonewolfcode2@gmail.com>
2024-08-09 09:45:49 -0500

    remove morphSVG

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,4 @@
-import Examples from "@/components/Examples";
 import LineDrawing from "@/components/LineDrawing";
-import { MotionPath } from "@/components/MotionPath";
-import ScrollLinked from "@/components/ScrollLinked";
-import MorphSvg from "@/components/MorphSvg";
 export default function Home() {
   return (
     <main className="bg-background min-h-screen bg-gradient-to-tr from-background to-primary grid place-items-center p-24">
